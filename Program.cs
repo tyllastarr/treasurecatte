171d101d3c83e26d9b0e38515959ded2bf65c62f
Tyll'a <psduckie@yahoo.com>
2026-07-20 22:27:44 -0400

    Added reset method

--- Program.cs
+++ Program.cs
@@ -1,7 +1,19 @@
-﻿namespace TreasureCatte;
+﻿using System;
+using System.Collections.Generic;
+
+namespace TreasureCatte;
 
 class Program
 {
+    static Dictionary<int,bool> numbers = new Dictionary<int, bool>();
+
+    static void ResetArray()
+    {
+        for(int i = 10; i < 100; i++)
+        {
+            numbers[i] = false;
+        }
+    }
     static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
